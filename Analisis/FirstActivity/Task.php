9faba223037ae0b053d7d88f024c635f75de81e9
Darwin Andres Torres Agrado <darwin97081@gmail.com>
2018-04-19 23:44:56 -0500

    First Activity PHP
    Print name with PHP

---
+++ Analisis/FirstActivity/Task.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace FirstActivity;
+
+class Task {
+    
+    public function name() {
+        echo "Darwin Andres Torres Agrado";
+    }
+    public function poo() {
+        echo 'This is my second message in PHP through object-oriented programming.';
+    }
+}
---
+++ Analisis/FirstActivity/index.php
@@ -0,0 +1,12 @@
+<?php
+
+require 'Task.php';
+
+use FirstActivity\Task;
+
+$Task = new task ();
+$Task->name();
+
+$message = new task ();
+$message->poo();
+?>
